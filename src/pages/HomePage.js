237e6c427a9750c16ae267a1081e5ccb8007b673
Leonardo Igor <115428616+leosportt@users.noreply.github.com>
2023-10-24 15:41:08 -0300

    Add files via upload

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { Button } from 'react-native-paper';
 
 const HomePage = () => {
 
 return (
 
-  <View> 
+  <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
   
     <Text> Puc Minas </Text>
+
+    <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+      Press me
+    </Button>
 
   </View>
 
